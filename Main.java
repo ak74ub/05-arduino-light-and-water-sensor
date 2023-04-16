3ad16befde046811a0e4e84a9e14d86b9c712b56
Tony <tuk92144@temple.edu>
2023-04-16 16:20:12 -0400

    Update Main.java
    new layout

--- Main.java
+++ Main.java
@@ -1,42 +1,50 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javafx.scene.Group;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.*;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 
 public class Main extends Application {
 Button button;
+Scene scene, scene1;
+
 
 public static void main(String[] args){
     launch(args); 
 }
+@Override
+public void start (Stage primaryStage) throws Exception{
+    GridPane grid = new GridPane();
+    grid.setPadding(new Insets(10,100,10,100));
+    grid.setVgap(8);
+    grid.setHgap(10);
+    //Menu
+    Label userLabel = new Label("Weclome to our Arduino Project!");
+    Button submitButton = new Button("Click here to start");
+    Label label1 = new Label();
 
-public void start(Stage primaryStage) throws Exception{
-    primaryStage.setTitle("Water, Light, and Soil Sensor");
-    Label welcome = new Label("Welcome to our group project");
-    welcome.relocate(10, 20);
+    HBox hBox = new HBox(100, userLabel);
+    VBox vBox = new VBox(100,hBox, submitButton, label1);
+    vBox.setAlignment(Pos.CENTER);
+    vBox.setPadding(new Insets(10));
+    submitButton.setOnAction(e->primaryStage.setScene(scene1));
 
-    button = new Button();
-    button.setText("Analyze");
 
-    StackPane layout = new StackPane();
-    layout.getChildren().add(button);
-    layout.getChildren().add(welcome);
-    
-    
-
-    Scene scene = new Scene(layout,600,400);
+    Scene scene = new Scene(vBox);
     primaryStage.setScene(scene);
+    primaryStage.setTitle("Arduino Project");
     primaryStage.show();
+    scene1 = new Scene(grid,700,400);
+
+
+
 }
     
 
