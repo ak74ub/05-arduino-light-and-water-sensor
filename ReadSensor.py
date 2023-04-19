dda1bd7c6cc726f9c3f02d8f77c28b4da3c4b00e
noise404d <im.still.awake404@gmail.com>
2023-04-19 10:24:53 -0400

    Fixed bug preventing python script to write to file while arduino is running

--- ReadSensor.py
+++ ReadSensor.py
@@ -2,12 +2,14 @@ import serial
 import time
 
 f = open("SensorData.txt", "w")
-SLEEPTIME = 60
+SLEEPTIME = 1
 sensorInfo = ""
 
-ser = serial.Serial('COM4', 9600, timeout=None) # COM4 is a stand in, actual port may vary
+serial.Serial('COM5').close() # not again!
+ser = serial.Serial('COM5', 115200, timeout=None) # COM4 is a stand in, actual port may vary
 while True: # maybe replace with an exit condition at some point
     sensorInfo = ser.readline().decode().strip()
+    print("Write")
     f.write(sensorInfo + "\n")
     f.close()
     time.sleep(SLEEPTIME) # sleep some amount of time, should depend on sensor read interval
