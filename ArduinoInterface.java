0f4947e84199c78cb95c11d1b2053d2fbd14da5f
Tony <tuk92144@temple.edu>
2023-04-24 18:05:52 -0400

    Update ArduinoInterface.java
    Deleted textArea 1 and added an if condition for soil

--- ArduinoInterface.java
+++ ArduinoInterface.java
@@ -29,30 +29,26 @@ public class ArduinoInterface extends Application {
         gridPane.setVgap(10);
 
         // Create a Label and add it to the GridPane
-        Label label2 = new Label("Soil Moisture:");
+        Label label2 = new Label("Welcome to our Software Design Project!");
         gridPane.add(label2, 0, 2);
 
-        // Create a TextArea for file 2 and add it to the GridPane
-        textArea2 = new TextArea();
-        gridPane.add(textArea2, 0, 3, 3, 1);
-
         // Create a Label and add it to the GridPane
-        Label label3 = new Label("Sun Lux Level:");
+        Label label3 = new Label("Data Value:");
         gridPane.add(label3, 0, 4);
 
         // Create a TextArea for file 3 and add it to the GridPane
         textArea3 = new TextArea();
         gridPane.add(textArea3, 0, 5, 3, 1);
 
-        // Create a Button to read the contents of soil
-        Button button2 = new Button("Soil moisture");
-        button2.setOnAction(event -> readFile("file2.txt", textArea2));
-        gridPane.add(button2, 1, 6);
+        // // Create a Button to read the contents of soil
+        // Button button2 = new Button("Soil moisture");
+        // button2.setOnAction(event -> readFile("file2.txt", textArea2));
+        // gridPane.add(button2, 1, 6);
 
         // Create a Button to read the contents of Sun
-        Button button3 = new Button("Sun Analyzer");
+        Button button3 = new Button("Analyze Data");
         button3.setOnAction(event -> findColumnAverages("SensorData.txt", textArea3));
-        gridPane.add(button3, 2, 6);
+        gridPane.add(button3, 0, 6);
 
         // Create a Scene and display it on the Stage
         Scene scene = new Scene(gridPane, 400, 400);
@@ -116,17 +112,24 @@ public class ArduinoInterface extends Application {
                 averages[i] = sums[i] / lines.size();
             }
     
-            // Display the column-wise averages in the TextArea
+            // Check if the average voltage capacity is greater than or less than 500, and display corresponding message
+            String voltageMessage = "Dry soil";
+            if (averages[2] > 500) {
+                voltageMessage = "Wet soil";
+            }
+    
+            // Display the column-wise averages and soil status in the TextArea
             textArea.setText("Average Temperature: " + averages[0] + "\n" +
                     "----------------------------------------------------------------"+"\n"+
                     "Average Lux: " + averages[1] + "\n" +
                     "----------------------------------------------------------------"+"\n"+
-                    "Average Voltage Capacity: " + averages[2]);
+                    "Average Voltage Capacity: " + averages[2] + "\n" +
+                    "----------------------------------------------------------------"+"\n"+
+                    "Soil status: " + voltageMessage);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
     
 
     public static void main(String[] args) {
