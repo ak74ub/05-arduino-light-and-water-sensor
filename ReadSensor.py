9cb1f8542b529692220a85c3c18601a10e8ead71
tuj91536 <tuj91536@temple.edu>
2023-04-24 21:49:54 -0400

    added exit condition to while loop

--- ReadSensor.py
+++ ReadSensor.py
@@ -1,15 +1,15 @@
 import serial
-import time
 
 f = open("SensorData.txt", "w")
 sensorInfo = ""
 
-ser = serial.Serial('COM3', 9600, timeout=None) # COM3 is the port used by Alex's computer. Might be different on other machines
+ser = serial.Serial('COM3', 9600, timeout=60) # COM3 is the port used by Alex's computer. Might be different on other machines
 
 # makeshift do while
 sensorInfo = ser.readline().decode().strip() 
-f.write(sensorInfo + "\n")
-while True: # maybe replace with an exit condition at some point
+sensorInfo += "\n"
+f.write(sensorInfo)
+while sensorInfo[-1] == "\n": # if readline times out no new line character will be read
     f.close() # close file while ser.readline() blocks
     sensorInfo = ser.readline().decode().strip() # blocking call. Sleep is not needed
     f = open("SensorData.txt", "a")
