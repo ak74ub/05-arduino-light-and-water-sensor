672d4e7270d604f8dc7654253b3726af846896b7
tuj91536 <tuj91536@temple.edu>
2023-04-24 21:15:33 -0400

    Wrapper for ReadSensor.py

--- launcher.java
+++ launcher.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class launcher {
     public static void main(String []args) throws IOException {
-        Runtime.getRuntime().exec("python ReadSensor.py");
+        Runtime.getRuntime().exec("python scriptrunner.py");
         ArduinoInterface.main(args);
     }
 }
